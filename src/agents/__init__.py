53d64a19cd13f59974ca414821f836d359491d50
James Newman <87392097+James-R-Newman@users.noreply.github.com>
2026-04-24 15:19:50 -0400

    Add QLearning and QLearningBonus imports

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,4 +1,6 @@
 from .n_step_sarsa import NStepSARSA
 from .sarsa import Sarsa
+from .q_learning import QLearning
+from .q_learning_bonus import QLearningBonus
 from .q_learning_eligibility_traces import QLambda
 from .sarsa_lambda_eligibility_traces import SarsaLambdaEligibilityTraces
